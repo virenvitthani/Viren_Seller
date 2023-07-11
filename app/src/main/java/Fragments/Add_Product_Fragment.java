242b74d8b0af661bd41d262078d2409c997b7498
virenvitthani <virenvitthani1410@gmail.com>
2023-07-11 17:14:54 +0530

    forth commit

--- app/src/main/java/Fragments/Add_Product_Fragment.java
+++ app/src/main/java/Fragments/Add_Product_Fragment.java
@@ -6,11 +6,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,20 +17,31 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.new_viren_seller.R;
+import com.example.new_viren_seller.Splash_Images;
 import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
+import DataBase.AddproductData;
+import DataBase.InstanceClass;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Add_Product_Fragment extends Fragment
 {
 
-    EditText pdname,pdprice,;
+    EditText pdname,pdprice,pddescription;
     Button submit;
     ImageView addcontactsImg;
     @SuppressLint("MissingInflatedId")
@@ -43,6 +53,7 @@ public class Add_Product_Fragment extends Fragment
         addcontactsImg = view.findViewById(R.id.addcontactsImg);
         pdname = view.findViewById(R.id.pdname);
         pdprice = view.findViewById(R.id.pdprice);
+        pddescription = view.findViewById(R.id.pddescription);
 
         submit = view.findViewById(R.id.submit);
 
@@ -55,24 +66,57 @@ public class Add_Product_Fragment extends Fragment
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bitmap bitmap = ((BitmapDrawable) addcontactsImg.getDrawable()).getBitmap();
 
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                // In case you want to compress your image, here it's at 40%
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 40, byteArrayOutputStream);
-                byte[] byteArray = byteArrayOutputStream.toByteArray();
+                Bitmap bm = ((BitmapDrawable) addcontactsImg.getDrawable()).getBitmap();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bm.compress(Bitmap.CompressFormat.JPEG, 40, baos); // bm is the bitmap object
+                byte[] b = baos.toByteArray();
 
+                String imagedata = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    imagedata = Base64.getEncoder().encodeToString(b);
+                }
 
+                addfragment(new Home_Fragment());
 
+                if(Splash_Images.preferences.getString("from",null).equals("add")) {
+                    InstanceClass.CallApi().addproduct(Splash_Images.preferences.getInt("userid", 0), pdname.getText().toString(), pdprice.getText(), pddescription.getText().toString(), imagedata).enqueue(new Callback<AddproductData>() {
+                        @Override
+                        public void onResponse(Call<AddproductData> call, Response<AddproductData> response) {
+                            if (response.body().getConnection() == 1) {
+                                if (response.body().getProductaddd() == 1) {
+                                    Toast.makeText(getContext(), "Product Add Sucessfully", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getContext(), "Failed to Add Product", Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Toast.makeText(getContext(), "Check Internet Connection", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<AddproductData> call, Throwable t) {
+
+                        }
+                    });
+                }
             }
         });
         return view;
     }
 
-     void imagechooser()
+    private void addfragment(Fragment fragment)
+    {
+        FragmentManager fm = getParentFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.framlayout,fragment);
+        transaction.commit();
+    }
+
+    void imagechooser()
     {
         CropImage.activity()
-                .start(getContext(), this);
+                .start(getContext(),this);
     }
 
 
@@ -83,7 +127,7 @@ public class Add_Product_Fragment extends Fragment
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 Uri resultUri = result.getUri();
-
+                addcontactsImg.setImageURI(resultUri);
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
--- app/src/main/java/Fragments/Inventory_Fragment.java
+++ app/src/main/java/Fragments/Inventory_Fragment.java
@@ -3,4 +3,5 @@ package Fragments;
 import androidx.fragment.app.Fragment;
 
 public class Inventory_Fragment extends Fragment {
+
 }
