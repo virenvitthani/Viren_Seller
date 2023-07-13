80d4d3e288a1d9930c999bd4d5e16672dfada9a3
virenvitthani <virenvitthani1410@gmail.com>
2023-07-13 13:27:27 +0530

    sixth commit

--- app/src/main/java/Fragments/Inventory_Fragment.java
+++ app/src/main/java/Fragments/Inventory_Fragment.java
@@ -1,7 +1,31 @@
 package Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.new_viren_seller.R;
+import com.example.new_viren_seller.ViewproAdapter;
 
 public class Inventory_Fragment extends Fragment {
 
+    ViewproAdapter adapter;
+    RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.inventory_fragment,container,false);
+
+        adapter=new ViewproAdapter(Inventory_Fragment.this);
+        recyclerView=view.findViewById(R.id.recycleview);
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
 }
---
+++ app/src/main/java/com/example/new_viren_seller/ViewproAdapter.java
@@ -0,0 +1,76 @@
+package com.example.new_viren_seller;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import DataBase.Productdatum;
+import Fragments.Inventory_Fragment;
+
+public class ViewproAdapter extends RecyclerView.Adapter<ViewproAdapter.proHolder> {
+
+    Inventory_Fragment inventory_fragment;
+    List<Productdatum> productdata;
+
+    public ViewproAdapter(Inventory_Fragment inventory_fragment) {
+        this.inventory_fragment = inventory_fragment;
+        this.productdata = productdata;
+    }
+
+    @NonNull
+    @Override
+    public ViewproAdapter.proHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_inventory_item,parent,false);
+        proHolder holder = new proHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewproAdapter.proHolder holder, int position) {
+        holder.pdname.setText("\t"+productdata.get(position).getPdname());
+        holder.pdprice.setText("pdprice"+productdata.get(position).getPdprice());
+        holder.pddescription.setText("pddescription"+productdata.get(position).getDescription());
+
+        Glide.with(inventory_fragment).load("https://mihirecommarce.000webhostapp.com/Mysite"+productdata.get(position).getImages()).into(holder.imageView);
+
+        holder.menuoption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Popup",Toast.LENGTH_LONG).show();
+                PopupMenu popupMenu = new PopupMenu(inventory_fragment.getContext(),v);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+                popupMenu.show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productdata.size();
+    }
+
+    public class proHolder extends RecyclerView.ViewHolder {
+        ImageView imageView,menuoption;
+        TextView pdname,pdprice,pddescription;
+
+        public proHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.inven_recyc_img);
+            menuoption = itemView.findViewById(R.id.inven_recyc_menuoption);
+            pdname = itemView.findViewById(R.id.inven_recyc_name);
+            pdprice = itemView.findViewById(R.id.inven_recyc_price);
+            pddescription = itemView.findViewById(R.id.inven_recyc_description);
+        }
+    }
+}
